500dce4c8cd21b11405b0c884946faa96b2bbe6e
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-04-21 19:32:33 +0800

    Update svg compatibility for safari. meta tag updates

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -8,7 +8,6 @@ const Layout = () => {
         <ul  className="nav-ul">
           <li className="nav">
             <a href="https://my-philippines-travel-level.com/">Inspired by My Philippines Travel Level</a>
-            <a href="">Source</a>
           </li>
         </ul>
       </nav>
--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import BoholMapJSX from '../BoholMap';
+import React, { useState } from "react";
+import BoholMapJSX from '../BoholMapJSX';
+//import BoholMapJSX from "../BoholMapTrial";
 import ".././App.css";
 
 const BoholMap = () => {
@@ -10,28 +11,33 @@ const BoholMap = () => {
   const [menuVisible, setMenuVisible] = useState(false);
   const [totalLevel, setTotalLevel] = useState(0);
 
-  const searchUrl = 'http://www.google.com/search?q="' + selectedProvince + ', Bohol fiesta date"';
+  const searchUrl =
+    'http://www.google.com/search?q="' +
+    selectedProvince +
+    ', Bohol fiesta date"';
 
   const menuOptions = [
-    { label: 'Nagpista sa inyo', level: 5, fill: '#e84c3d' },
-    { label: 'Naka-bringhouse', level: 4, fill: '#d58337' },
-    { label: 'Naka disco', level: 3, fill: '#f3c218' },
-    { label: 'Nakapamista ug daghang balay', level: 2, fill: '#30cc70' },
-    { label: 'Nakapamista', level: 1, fill: '#3598db' },
-    { label: 'Wala kapamista', level: 0, fill: 'white' },
+    { label: "Nagpista sa inyo", level: 5, fill: "#e84c3d" },
+    { label: "Naka-bringhouse", level: 4, fill: "#d58337" },
+    { label: "Naka disco", level: 3, fill: "#f3c218" },
+    { label: "Nakapamista ug daghang balay", level: 2, fill: "#30cc70" },
+    { label: "Nakapamista", level: 1, fill: "#3598db" },
+    { label: "Wala kapamista", level: 0, fill: "white" },
   ];
 
   const handleLevelClick = (selectedProvince, selectedProvinceLayer, event) => {
-    const newLevel = parseInt(event.target.getAttribute('level'));
-    const provinceIndex = provinceLevels
-      .findIndex((province) => province.id === selectedProvince);
+    const newLevel = parseInt(event.target.getAttribute("level"));
+    const provinceIndex = provinceLevels.findIndex(
+      (province) => province.id === selectedProvince
+    );
     if (provinceIndex !== -1) {
       provinceLevels[provinceIndex].level = newLevel;
     } else {
       provinceLevels.push({ id: selectedProvince, level: newLevel });
     }
 
-    const fill = menuOptions.find((option) => option.level === newLevel)?.fill || 'white';
+    const fill =
+      menuOptions.find((option) => option.level === newLevel)?.fill || "white";
     selectedProvinceLayer.style.fill = fill;
 
     setTotalLevel(provinceLevels.reduce((a, v) => a + v.level, 0));
@@ -50,19 +56,30 @@ const BoholMap = () => {
       />
       {menuVisible && (
         <div
-          className='level-menu'
+          className="level-menu"
           style={{
-            position: 'absolute',
+            position: "absolute",
             top: menuPosition.y,
             left: menuPosition.x,
-          }} >
+          }}
+        >
           <div>
-            <div className='menu-header' onClick={() => window.open(searchUrl)}>{selectedProvince} ↗ </div>
+            <div className="menu-header" onClick={() => window.open(searchUrl)}>
+              {selectedProvince} ↗{" "}
+            </div>
             {menuOptions.map(({ label, level }) => (
-              <div key={level}
+              <div
+                key={level}
                 level={level}
                 className={`level-${level}`}
-                onClick={(event) => handleLevelClick(selectedProvince, selectedProvinceLayer, event)}>
+                onClick={(event) =>
+                  handleLevelClick(
+                    selectedProvince,
+                    selectedProvinceLayer,
+                    event
+                  )
+                }
+              >
                 {label}
               </div>
             ))}
