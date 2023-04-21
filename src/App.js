fb3a1bfeba358f2ba2def98371d4c5cdc5b3af01
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-04-21 08:33:29 +0800

    Remove react router usage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MapPage from "./pages/MapPage";
 
 function App() {
   return (
-    <div className="App">
-    <BrowserRouter>
-      <Routes>
-          <Route index element={<MapPage />} />
-      </Routes>
-    </BrowserRouter>
-  </div>
+    <MapPage />
   );
 }
 
